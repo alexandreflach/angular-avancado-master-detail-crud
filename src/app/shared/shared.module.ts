d3929151755a0cbfe3c1a4b3f1180f6d0fa81c05
alexandreflach <contato@alexandreflach.com.br>
2018-12-17 21:54:55 -0200

    Utilizando o componente 'Migalha de Pão' nas Páginas do Projeto

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [BreadCrumbComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    BreadCrumbComponent
+    RouterModule
   ],
   exports: [
+    //shared modules
     CommonModule,
     ReactiveFormsModule,
+    RouterModule,
+
+    //shared components
     BreadCrumbComponent
   ]
 })
